4871fba96cc8f6efb2cc6e4309d3afd78e5fb37d
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-05-15 21:19:35 +0300

    Completed the test lab

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,9 +1,10 @@
+// import the libraries for tetsting
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-
+// import the component
 import Header from "../components/Header";
-
+// test the component
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
 
